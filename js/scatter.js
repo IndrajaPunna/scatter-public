6a3ce9249b8ad949e6ff503433490b7f289ebacd
praveen <apnerve@gmail.com>
2015-01-18 17:45:52 +0530

    animated scroll

--- js/scatter.js
+++ js/scatter.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
-  $('#hero').height($(window).height());
-  $(".header").pin();
+  $('#hero').height($(window).height()); // hero height
+  $(".header").pin(); // sticky nav
+  
+  // animated scroll
+  $('a[href^="#"]').on('click', function(event) {
+    var target = $(this.href.substring(this.href.indexOf('#')));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+  });
 });
