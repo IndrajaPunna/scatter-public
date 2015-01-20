44271b353d8c1576ea265de8f0f291fc738f9a04
praveen <apnerve@gmail.com>
2015-01-20 17:47:15 +0530

    edits

--- js/scatter.js
+++ js/scatter.js
@@ -1,15 +1,16 @@
 $(document).ready(function() {
   $('#hero').height($(window).height()); // hero height
+  
   $(".header").pin(); // sticky nav
   
   // animated scroll
   $('a[href^="#"]').on('click', function(event) {
     var target = $(this.href.substring(this.href.indexOf('#')));
     if( target.length ) {
-        event.preventDefault();
-        $('html, body').animate({
-            scrollTop: target.offset().top
-        }, 1000);
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top
+      }, 1000);
     }
   });
 });
